11a74f2edd217dda88cb5c3ff7b7fd1af873a5b6
devWaz <fawazodessy@gmail.com>
2022-10-19 23:32:16 +0100

    BRB

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,10 +1,14 @@
 import styled from "styled-components";
 import moment from "moment/moment";
-import { Link } from "react-router-dom";
+import Avatar from '@mui/material/Avatar';
+import {Select , Col} from "antd";
+
 
 import { useGetCryptoNewsQuery } from "../Services/NewsApi";
+// import { useGetCryptoDataQuery } from "../Services/CryptoApi";
+const {Option} = Select;
 
-const defaultImage = "http://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
+const defaultImage = "https://coinrevolution.com/wp-content/uploads/2020/06/cryptonews.jpg";
 
 const News = ({simplified}) => {
 
@@ -13,29 +17,57 @@ const News = ({simplified}) => {
         flex-wrap: wrap;
         gap: 1rem;
         padding: 1rem;
+        /* border: 1px solid blue; */
 
         a {
-            width: 32%;
+            width: 29%;
             text-decoration: none;
             color: white;
             background: #2d2d2d;
+            padding: 1rem;
+            display: flex;
+            flex-direction: column;
+            /* height: 35vh;
+            min-height: 35vh; */
         }
     `
 
     const Card = styled.div`
-        /* height: 20vh; */
-        padding: 1rem;
-
         .news {
             display: flex;
-            margin-bottom: 1rem;
+            /* border: 1px solid blue; */
+            margin-bottom: .7rem;
             h1 {
                 font-size: 1rem;
+            }
+            img {
+                height: 100px;
+                width: 100px;
+                max-width: 100px;
+                max-height: 100px;
             }
         }
 
         p {
             font-size: .8rem;
+        }
+
+        .provider { 
+            /* border: 1px solid red; */
+            display: flex;
+            align-items: center;
+            margin-top: 1rem;
+        }
+
+        .avatar {
+                width: 25px;
+                height: 25px;
+                margin-right: .5rem;
+        }
+
+        .datePublished {
+            margin-left: auto;
+            font-weight: 700;
         }
     `
 
@@ -45,6 +77,13 @@ const News = ({simplified}) => {
 
     return ( 
         <Container>
+            {!simplified && (
+                <div>
+                    <Select>
+                        <Option></Option>
+                    </Select>
+                </div>
+            )}
             {cryptoNews.value.map((news , i) => (
                     <a href={news.url} target="_blank" rel="noreferrer">
                         <Card>
@@ -53,6 +92,11 @@ const News = ({simplified}) => {
                                 <img src={news?.image?.thumbnail?.contentUrl || defaultImage} alt="News Image" />
                             </div>
                             <p>{news.description > 100 ? `${news.description.substring(0 , 100)}...` : news.description}</p>
+                            <div className="provider">
+                            <Avatar src={news.provider[0]?.image?.thumbnail?.contentUrl} className="avatar"/>
+                            <p>{news.provider[0]?.name}</p>
+                            <p className="datePublished">{moment(news.datePublished).startOf("ss").fromNow()}</p>
+                            </div>
                         </Card>
                     </a>    
             ))}
